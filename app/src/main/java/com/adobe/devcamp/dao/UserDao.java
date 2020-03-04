52d0c63b99fec6cb85d1a0de303ce322ae5efb89
Anca Enache <ancamihaela91@yahoo.com>
2020-03-04 20:31:48 +0200

    Added model.

---
+++ app/src/main/java/com/adobe/devcamp/dao/UserDao.java
@@ -0,0 +1,54 @@
+package com.adobe.devcamp.dao;
+
+import com.adobe.devcamp.model.Advertiser;
+import com.adobe.devcamp.model.Campaign;
+import com.adobe.devcamp.model.Publisher;
+import com.adobe.devcamp.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserDao {
+
+    private final Logger logger = LoggerFactory.getLogger(UserDao.class);
+    //aceeasi pentru orice instanta
+    private static final Map<Class, String> TABLES = new HashMap<>();
+    private final Connection connection;
+
+    static {
+        TABLES.put(User.class, "users");
+        TABLES.put(Advertiser.class, "advertisers");
+        TABLES.put(Publisher.class, "publishers");
+        TABLES.put(Campaign.class, "campaigns");
+    }
+
+    public UserDao(DataSource dataSource) throws SQLException {
+        //ne ocupam local daca folosim try-catch
+        this.connection = dataSource.getConnection();
+    }
+
+    public Map<Integer, String> selectAll() {
+        Map<Integer, String> all = new HashMap<>();
+        final String query = "SELECT * FROM " + TABLES.get(User.class);
+
+        try {
+            final Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                all.put(resultSet.getInt(1), resultSet.getString(2));
+            }
+        } catch (SQLException ex) {
+            logger.error("Query {} failed because {}", query, ex.getMessage());
+        }
+
+        return all;
+    }
+}
---
+++ app/src/main/java/com/adobe/devcamp/model/Campaign.java
@@ -0,0 +1,120 @@
+package com.adobe.devcamp.model;
+
+import com.sun.tools.javac.jvm.Gen;
+
+import java.lang.annotation.Target;
+import java.util.List;
+import java.util.Objects;
+
+public final class Campaign {
+    private final String name;
+    private final Long startTime;
+    private final Long endTime;
+    private final Target target;
+    private final Integer advertiserId;
+
+    public Campaign(String name, Long startTime, Long endTime, Target target, Integer advertiserId) {
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.target = target;
+        this.advertiserId = advertiserId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Long getStartTime() {
+        return startTime;
+    }
+    public Long getEndTime() {
+        return endTime;
+    }
+    public Target getTarget() {
+        return target;
+    }
+    public Integer getAdvertiserId() {
+        return advertiserId;
+    }
+
+    public static class Target {
+        private final Gender gender;
+        private final List<Domain> interests;
+        private final Short minAge;
+        private final Short maxAge;
+
+        public Target(Gender gender, List<Domain> interests, Short minAge, Short maxAge) {
+            this.gender = gender;
+            this.interests = interests;
+            this.minAge = minAge;
+            this.maxAge = maxAge;
+        }
+
+        public Gender getGender() {
+            return gender;
+        }
+        public List<Domain> getInterests() {
+            return interests;
+        }
+        public Short getMinAge() {
+            return minAge;
+        }
+        public Short getMaxAge() {
+            return maxAge;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Target target = (Target) o;
+            return getGender() == target.getGender() &&
+                    Objects.equals(getInterests(), target.getInterests()) &&
+                    Objects.equals(getMinAge(), target.getMinAge()) &&
+                    Objects.equals(getMaxAge(), target.getMaxAge());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getGender(), getInterests(), getMinAge(), getMaxAge());
+        }
+
+        @Override
+        public String toString() {
+            return "Target{" +
+                    "gender=" + gender +
+                    ", interests=" + interests +
+                    ", minAge=" + minAge +
+                    ", maxAge=" + maxAge +
+                    '}';
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Campaign campaign = (Campaign) o;
+        return Objects.equals(getName(), campaign.getName()) &&
+                Objects.equals(getStartTime(), campaign.getStartTime()) &&
+                Objects.equals(getEndTime(), campaign.getEndTime()) &&
+                Objects.equals(getTarget(), campaign.getTarget()) &&
+                Objects.equals(getAdvertiserId(), campaign.getAdvertiserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getStartTime(), getEndTime(), getTarget(), getAdvertiserId());
+    }
+
+    @Override
+    public String toString() {
+        return "Campaign{" +
+                "name='" + name + '\'' +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", target=" + target +
+                ", advertiserId=" + advertiserId +
+                '}';
+    }
+}
