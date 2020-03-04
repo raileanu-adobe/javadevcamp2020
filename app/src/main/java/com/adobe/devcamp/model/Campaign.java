74cc952a3bc05a13e72eb3e98504bdfe68646667
Lidia Istrate <lidia.istrate2807@stud.acs.upb.ro>
2020-03-04 20:33:47 +0200

    Day1

---
+++ app/src/main/java/com/adobe/devcamp/model/Campaign.java
@@ -0,0 +1,124 @@
+package com.adobe.devcamp.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Campaign {
+    private final String name;
+    private final Long startTime;
+    private final Long endTime;
+    private final Target target;
+    private final Integer advertiserId;
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getStartTime() {
+        return startTime;
+    }
+
+    public Long getEndTime() {
+        return endTime;
+    }
+
+    public Target getTarget() {
+        return target;
+    }
+
+    public Integer getAdvertiserId() {
+        return advertiserId;
+    }
+
+    public Campaign(String name, Long startTime, Long endTime, Target target, Integer advertiserId) {
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.target = target;
+        this.advertiserId = advertiserId;
+    }
+
+    @Override
+    public String toString() {
+        return "Campaign{" +
+                "name='" + name + '\'' +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", target=" + target +
+                ", advertiserId=" + advertiserId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Campaign campaign = (Campaign) o;
+        return Objects.equals(getName(), campaign.getName()) &&
+                Objects.equals(getStartTime(), campaign.getStartTime()) &&
+                Objects.equals(getEndTime(), campaign.getEndTime()) &&
+                Objects.equals(getTarget(), campaign.getTarget()) &&
+                Objects.equals(getAdvertiserId(), campaign.getAdvertiserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getStartTime(), getEndTime(), getTarget(), getAdvertiserId());
+    }
+
+    public static final class Target {
+        private final Gender gender;
+        private final List<Domain> interests;
+        private final short minAge;
+        private final short maxAge;
+
+        public Target(Gender gender, List<Domain> interests, short minAge, short maxAge) {
+            this.gender = gender;
+            this.interests = interests;
+            this.minAge = minAge;
+            this.maxAge = maxAge;
+        }
+
+        public Gender getGender() {
+            return gender;
+        }
+
+        public List<Domain> getInterests() {
+            return interests;
+        }
+
+        public short getMinAge() {
+            return minAge;
+        }
+
+        public short getMaxAge() {
+            return maxAge;
+        }
+
+        @Override
+        public String toString() {
+            return "Target{" +
+                    "gender=" + gender +
+                    ", interests=" + interests +
+                    ", minAge=" + minAge +
+                    ", maxAge=" + maxAge +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Target target = (Target) o;
+            return getMinAge() == target.getMinAge() &&
+                    getMaxAge() == target.getMaxAge() &&
+                    getGender() == target.getGender() &&
+                    Objects.equals(getInterests(), target.getInterests());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getGender(), getInterests(), getMinAge(), getMaxAge());
+        }
+    }
+}
