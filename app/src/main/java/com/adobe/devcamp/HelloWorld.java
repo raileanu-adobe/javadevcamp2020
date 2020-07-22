e79494f87b414346b37fa0bf4f28a7c993a59b98
Anca Enache <ancamihaela91@yahoo.com>
2020-07-22 17:45:20 +0300

    Day 3 changes added.

--- app/src/main/java/com/adobe/devcamp/HelloWorld.java
+++ app/src/main/java/com/adobe/devcamp/HelloWorld.java
@@ -20,8 +20,12 @@
 
 package com.adobe.devcamp;
 
+import com.adobe.devcamp.model.Advertiser;
+import com.adobe.devcamp.model.Campaign;
+import com.adobe.devcamp.model.Publisher;
 import com.adobe.devcamp.model.User;
-import com.adobe.devcamp.service.UserService;
+import com.adobe.devcamp.service.AdvertisingService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -31,19 +35,47 @@ import java.util.Map;
 public class HelloWorld {
 
 
-    private static UserService userService;
+    private static AdvertisingService<User> userService;
+    private static AdvertisingService<Publisher> publisherService;
+    private static AdvertisingService<Advertiser> advertisingService;
+    private static AdvertisingService<Campaign> campaignService;
 
-    public HelloWorld(UserService userService) {
+    private static ObjectMapper objectMapper;
+
+    public HelloWorld(AdvertisingService<User> userService,
+                      AdvertisingService<Publisher> publisherService,
+                      AdvertisingService<Advertiser> advertisingService,
+                      AdvertisingService<Campaign> campaignService,
+                      ObjectMapper objectMapper) {
         HelloWorld.userService = userService;
+        HelloWorld.publisherService = publisherService;
+        HelloWorld.advertisingService = advertisingService;
+        HelloWorld.campaignService = campaignService;
+        HelloWorld.objectMapper = objectMapper;
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         SpringApplication.run(HelloWorld.class);
-        System.out.println("Hello World");
-        final Map<Integer, User> users = userService.selectAll();
+        System.out.println("\nHello World");
+        final Map<Integer, User> users = userService.selectAll(User.class);
         users.entrySet().forEach(entry->System.out.println(entry.getKey()+"-"+entry.getValue()));
-    }
 
+        System.out.println("\nUser with id: 1");
+        User user = userService.selectById(User.class, 1);
+        System.out.println(objectMapper.writeValueAsString(user));
+
+        System.out.println("\nPublishers");
+        final Map<Integer, Publisher> publishers = publisherService.selectAll(Publisher.class);
+        publishers.entrySet().forEach(entry->System.out.println(entry.getKey()+"-"+entry.getValue()));
+
+        System.out.println("\nAdvertisers");
+        final Map<Integer, Advertiser> advertisers = advertisingService.selectAll(Advertiser.class);
+        advertisers.entrySet().forEach(entry->System.out.println(entry.getKey()+"-"+entry.getValue()));
+
+        System.out.println("\nCampaigns");
+        final Map<Integer, Campaign> campaigns = campaignService.selectAll(Campaign.class);
+        campaigns.entrySet().forEach(entry->System.out.println(entry.getKey()+"-"+entry.getValue()));
+    }
 
 }
