a543e7eb6b5a9910ef4fe4b33747dca27198fd78
raileanu <raileanu@adobe.com>
2020-07-23 10:32:42 +0300

    local config

--- app/src/main/java/com/adobe/devcamp/config/BeanConfig.java
+++ app/src/main/java/com/adobe/devcamp/config/BeanConfig.java
@@ -33,9 +33,9 @@ public class BeanConfig {
     @Bean
     public DataSource dataSource(){
         final MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/javadevcamp");
-        dataSource.setUser("root");
-        dataSource.setPassword("");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/javadevcamp?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        dataSource.setUser("jdc");
+        dataSource.setPassword("devcamp");
         return dataSource;
     }
 
